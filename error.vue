8fa84e20a56fca877f994cee6f5f8e8edea92857
Cyrus Yip <cyruscmyip1@gmail.com>
2024-06-07 00:23:45 -0700

    add error page

---
+++ error.vue
@@ -0,0 +1,29 @@
+<template>
+  <NuxtLayout>
+    <div class="auto-mx">
+      <div v-if="error?.statusCode == 404">
+        <h1 class="title mb-5">Page not found</h1>
+        <p class="text-xl sm:text-2xl">
+          Sorry, the page you are looking for does not exist.
+          <SimpleLink to="/">Back to home.</SimpleLink>
+        </p>
+      </div>
+      <div v-else>
+        <h1 class="title mb-5">Error {{ error?.statusCode }}</h1>
+        <p class="text-xl sm:text-2xl">Something is terribly wrong! 😞</p>
+      </div>
+    </div>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+const props = defineProps({
+  error: Object as () => NuxtError,
+});
+
+useSeoMeta({
+  title: `Error ${props.error?.statusCode}`,
+});
+</script>
