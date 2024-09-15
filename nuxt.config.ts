f5b877a1f3b5aa5e1fd37d367e7fe73bba6a2792
Cyrus Yip <cyrusyip@cyrusyip.com>
2024-09-15 00:18:52 -0400

    update to @nuxt/icon 1.5.1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
     "@nuxtjs/tailwindcss",
     "@nuxtjs/google-fonts",
     "@formkit/nuxt",
-    "nuxt-icon",
+    "@nuxt/icon",
     "@nuxtjs/seo",
     "@hypernym/nuxt-gsap",
   ],
