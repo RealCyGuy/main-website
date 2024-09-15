175c6d9d0d45748ed5897af56e4cb4d8b4cda041
Cyrus Yip <cyrusyip@cyrusyip.com>
2024-09-15 01:50:00 -0400

    decrease font size of largest footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,10 +4,10 @@
       class="flex flex-col justify-between h-dvh bg-black fixed bottom-0 w-full p-5 sm:p-12 md:py-10 md:px-14 lg:py-16 lg:px-20 gap-5"
     >
       <div class="flex flex-col gap-7">
-        <div class="text-5xl sm:text-7xl lg:text-9xl flex flex-col lg:block">
+        <div class="text-5xl sm:text-7xl lg:text-8xl flex flex-col lg:block">
           <span>Let's</span> <span><span class="verb">talk</span>.</span>
         </div>
-        <p class="text-xl sm:text-3xl lg:text-5xl">
+        <p class="text-xl sm:text-3xl lg:text-4xl">
           <SimpleLink to="/contact">Contact me</SimpleLink> or email me
           <SimpleLink to="mailto:cyrusyip@cyrusyip.com"
             >cyrusyip@cyrusyip.com</SimpleLink
@@ -15,7 +15,7 @@
         </p>
       </div>
       <div
-        class="flex min-h-0 flex-col gap-2 sm:gap-5 text-3xl sm:text-5xl lg:text-7xl items-start flex-wrap"
+        class="flex min-h-0 flex-col gap-2 sm:gap-5 text-3xl sm:text-5xl lg:text-6xl items-start flex-wrap"
       >
         <ComplexLink to="/">About</ComplexLink>
         <ComplexLink to="/projects">Projects</ComplexLink>
@@ -23,7 +23,7 @@
         <ComplexLink to="/contact">Contact</ComplexLink>
         <ComplexLink to="/square">Square</ComplexLink>
       </div>
-      <div class="flex flex-col gap-5 text-xl sm:text-3xl lg:text-5xl">
+      <div class="flex flex-col gap-5 text-xl sm:text-3xl lg:text-4xl">
         <p>
           <SimpleLink to="https://v1--realcyguy.netlify.app">v1</SimpleLink> -
           <SimpleLink to="https://v2--realcyguy.netlify.app">v2</SimpleLink> -
