67ae534de1750b19a87e1cd312fc345647ebbb32
Cyrus Yip <cyrusyip@cyrusyip.com>
2024-11-04 19:57:13 -0500

    also change footer link icon opacity on hover

--- components/ComplexLink.vue
+++ components/ComplexLink.vue
@@ -4,7 +4,7 @@
     class="flex items-center overflow-hidden py-0.5 relative group pr-16"
   >
     <Icon
-      class="absolute -left-16 rotate-90 group-hover:rotate-0 group-hover:translate-x-16 transition-transform duration-300 ease-out w-12"
+      class="absolute -left-16 rotate-90 opacity-0 group-hover:opacity-100 group-hover:rotate-0 group-hover:translate-x-16 transition-all duration-300 ease-out w-12"
       name="material-symbols:arrow-circle-right"
     />
     <span
